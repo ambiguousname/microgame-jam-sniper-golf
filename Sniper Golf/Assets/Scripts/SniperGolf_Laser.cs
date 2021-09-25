5bbf68b3bf25127ad929acfa6ac38a5b7f744aa3
ambiguousname <tyly04@gmail.com>
2021-09-25 16:00:15 -0700

    Fixed laser rotation

--- Sniper Golf/Assets/Scripts/SniperGolf_Laser.cs
+++ Sniper Golf/Assets/Scripts/SniperGolf_Laser.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class SniperGolf_Laser : MonoBehaviour
 {
     [SerializeField] private Transform crosshair;
-    [SerializeField] private Transform pivotPoint;
     // Start is called before the first frame update
     void Start()
     {
         crosshair = GameObject.Find("Crosshair").transform;
-        pivotPoint = GameObject.Find("Pivot").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 rotateDirection = crosshair.position - this.transform.position;
-        Quaternion dir = Quaternion.LookRotation(rotateDirection);
-        this.transform.RotateAround(pivotPoint.localPosition, Vector3.forward, Quaternion.Angle(this.transform.rotation, dir));
+        float angle = Mathf.Atan2(rotateDirection.x, rotateDirection.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
     }
 }
