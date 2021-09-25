66ab2719f00ab3224b3c01a260f06f8e40b6ad83
ambiguousname <tyly04@gmail.com>
2021-09-25 15:47:13 -0700

    Revert "Fixed lasers"
    This reverts commit a7176c5ca9255019086a1e0754bd3765f971f911.

--- Sniper Golf/Assets/Scripts/SniperGolf_Laser.cs
+++ Sniper Golf/Assets/Scripts/SniperGolf_Laser.cs
@@ -9,12 +9,15 @@ public class SniperGolf_Laser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        crosshair = GameObject.Find("Crosshair").transform;
+        pivotPoint = GameObject.Find("Pivot").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 rotateDirection = crosshair.position - this.transform.position;
+        Quaternion dir = Quaternion.LookRotation(rotateDirection);
+        this.transform.RotateAround(pivotPoint.localPosition, Vector3.forward, Quaternion.Angle(this.transform.rotation, dir));
     }
 }
