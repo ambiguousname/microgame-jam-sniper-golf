1a998fea8d352279bf0f0a0bdd2d1c61260f50d5
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2021-09-25 12:55:21 -0700

    start new generator class

---
+++ Sniper Golf/Assets/SniperGolf_LevelGen/SniperGolf_LevelGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperGolf_LevelGenerator
+{
+    private SniperGolf_Node[,] grid;
+
+    [SerializeField] private int width = 4;
+    [SerializeField] private int height = 3;
+
+    public void Generate()
+    {
+        
+    }
+}
