962463382233d30ba6081b491c5277f702f200ec
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2021-09-25 13:10:18 -0700

    basic random generation from prefabs

--- Sniper Golf/Assets/SniperGolf_LevelGen/SniperGolf_LevelGenerator.cs
+++ Sniper Golf/Assets/SniperGolf_LevelGen/SniperGolf_LevelGenerator.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class SniperGolf_LevelGenerator
 {
-    private SniperGolf_Node[,] grid;
+    private SniperGolf_Segment[,] grid;
+
+    [SerializeField] private SniperGolf_Segment[] segmentOptions;
 
     [SerializeField] private int width = 4;
     [SerializeField] private int height = 3;
+    [SerializeField] private float spacing = 1;
 
     public void Generate()
     {
-        
+        grid = new SniperGolf_Segment[width, height];
+
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+            {
+                Vector3 position = new Vector3 (i * spacing, j * spacing, 0);
+                Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 4)*90f);
+                grid[i, j] = Instantiate(segmentOptions[Random.Range(0, segmentOptions.Length)], position, rotation);
+            }
     }
 }
