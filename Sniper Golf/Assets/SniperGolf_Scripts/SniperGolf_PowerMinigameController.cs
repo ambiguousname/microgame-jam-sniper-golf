a99fb2b15616d4a66e4613c798bf55bdfb01b59b
ambiguousname <tyly04@gmail.com>
2021-09-25 11:35:13 -0700

    Started adding the power minigame scripts and prefab

---
+++ Sniper Golf/Assets/SniperGolf_Scripts/SniperGolf_PowerMinigameController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperGolf_PowerMinigameController : MonoBehaviour
+{
+    public GameObject startY;
+    public GameObject endY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
