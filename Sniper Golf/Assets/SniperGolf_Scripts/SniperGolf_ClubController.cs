75c15accac56992f47e4c79cb170c5a4270a1952
ambiguousname <tyly04@gmail.com>
2021-09-25 16:47:18 -0700

    Tweaking how the golf club looks

--- Sniper Golf/Assets/SniperGolf_Scripts/SniperGolf_ClubController.cs
+++ Sniper Golf/Assets/SniperGolf_Scripts/SniperGolf_ClubController.cs
@@ -6,12 +6,26 @@ public class SniperGolf_ClubController : MonoBehaviour
 {
     public Transform clubPosition;
     public SniperGolf_GolfHit golfController;
+    SniperGolf_PowerMinigameController minigame;
     Vector3 targetPos;
     Vector3 targRot;
+    Vector3 golfSwingRot;
+    float snapBackSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        minigame = golfController.powerMinigame;
+        minigame.hitSpaceUpdate.AddListener(AddToTargetRot);
+        minigame.hitSpace.AddListener(SnapBack);
+    }
+
+    void AddToTargetRot(float progress) {
+        golfSwingRot = new Vector3(0, 0, -progress * 90);
+    }
+
+    void SnapBack(float progress) {
+        golfSwingRot = Vector3.zero;
+        snapBackSpeed = 5.0f;
     }
 
     // Update is called once per frame
@@ -19,7 +33,7 @@ public class SniperGolf_ClubController : MonoBehaviour
     {
         if (golfController.canHit)
         {
-            targetPos = golfController.transform.position - (golfController.transform.position - golfController.player.transform.position).normalized;
+            targetPos = golfController.transform.position - (golfController.transform.position - golfController.player.transform.position).normalized *0.3f;
             var targetRot = golfController.transform.position - this.transform.position;
             var angle = Mathf.Atan2(targetRot.y, targetRot.x) * Mathf.Rad2Deg;
             targRot = new Vector3(0, 0, angle);
@@ -31,6 +45,10 @@ public class SniperGolf_ClubController : MonoBehaviour
         }
         var target = Vector3.Lerp(this.transform.position, targetPos, Time.deltaTime * 3);
         this.transform.position = new Vector3(target.x, target.y, this.transform.position.z);
-        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(targRot), Time.deltaTime * 3);
+        if (snapBackSpeed > 1) {
+            snapBackSpeed -= Time.deltaTime * 3;
+        }
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(targRot + golfSwingRot), Time.deltaTime * 3 * snapBackSpeed);
     }
 }
