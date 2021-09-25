118a0acad59c2e6420754913580a1df084761bda
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2021-09-25 14:51:12 -0700

    add some terrain segments

--- Sniper Golf/Assets/SniperGolf_LevelGen/SniperGolf_LevelGenerator.cs
+++ Sniper Golf/Assets/SniperGolf_LevelGen/SniperGolf_LevelGenerator.cs
@@ -12,6 +12,7 @@ public class SniperGolf_LevelGenerator : MonoBehaviour
     [SerializeField] private int width = 4;
     [SerializeField] private int height = 3;
     [SerializeField] private float spacing = 1;
+    [SerializeField] private Transform gridStart;
 
     private void Start()
     {
@@ -25,9 +26,9 @@ public class SniperGolf_LevelGenerator : MonoBehaviour
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
             {
-                Vector3 position = new Vector3 (i * spacing, j * spacing, 0);
+                Vector3 position = new Vector3 (i * spacing, j * spacing, 0) + gridStart.position;
                 Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 4)*90f);
-                grid[i, j] = Instantiate(segmentOptions[Random.Range(0, segmentOptions.Length)], position, rotation);
+                grid[i, j] = (SniperGolf_Segment) Instantiate(segmentOptions[Random.Range(0, segmentOptions.Length)], position, rotation);
             }
     }
 }
